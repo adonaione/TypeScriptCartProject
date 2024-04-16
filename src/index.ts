189ff41ad023de7eb885ec6cb2a4630c0f4ae2a9
Adonai Romero <adonai@Adonais-MacBook-Pro.local>
2024-04-16 08:38:33 -0600

    sign in function not working

--- src/index.ts
+++ src/index.ts
@@ -1,123 +1,5 @@
 import {v4 as uuidv4 } from 'uuid';
 
-//  Create a Type to describe a Item
-// type Item = {
-//     readonly id: string;
-//     name: string;
-//     price: number;
-//     description: string;
-// }
-
-//  Create a Type to describe a User //
-// type User = {
-//     readonly id: string;
-//     name: string;
-//     age: number;
-//     cart: Item[];
-// }
-
-// create function createUser
-// function createUser(name: string, age: number): User {
-//     const newUser:User = {
-//         id: uuidv4(),
-//         name,
-//         age,
-//         cart: []
-//     }
-//     return newUser;
-// }
-
-// create function createItem
-// function createItem(name: string, price: number, description: string): Item {
-//     const newItem:Item = {
-//         id: uuidv4(),
-//         name,
-//         price,
-//         description
-//     }
-//     return newItem;
-// }
-
-// create function addToCart
-// function addToCart(user: User, item: Item):void{
-//     user.cart.push(item);
-//     console.log(`${item.name} has been added to ${user.name}'s cart.`)
-// }
-
-// create function removeFromCart
-// function removeFromCart(user: User, item: Item):void{
-//     user.cart = user.cart.filter( cartItem => cartItem.id !== item.id);
-//     console.log(`All ${item.name}'s have been removed from ${user.name}'s cart.`)
-// }
-
-// create function removeQuantityFromCart
-// function removeQuantityFromCart(user: User, item: Item, quantity: number):void{
-//     for (let i=0; i < quantity; i++) {
-//         let indexofItem = user.cart.findIndex(cartItem => cartItem.id === item.id);
-//         user.cart.splice(indexofItem, 1);
-//         }
-//         console.log(`${quantity} ${item.name}'s have been removed from ${user.name}'s cart.`)
-// }  
-
-// create function cartTotal
-// function cartTotal(user: User):number{
-//     let total = 0;
-//     for (let i=0; i < user.cart.length; i++) {
-//         total = total + user.cart[i].price;
-//     }
-//     return total;
-    
-// }
-
-// create function printCart
-// function printCart(user: User):void{
-//     console.log(`${user.name}'s cart:`)
-//     for (let item of user.cart) {
-//         console.log(item.name)
-//     }
-//     console.log(`The total cost of ${user.name}'s cart is $${cartTotal(user)}.`)
-// }
-
-
-
-// let customer1 = createUser('Adonai', 26);
-
-// let quinoa = createItem('Quinoa', 25.00, 'Organic white quinoa, unwashed');
-
-// let housePlant = createItem('Monstera Leaf Plant', 10.00, 'A beautiful, large leafed house plant');
-
-// let yogaMat = createItem('Cork Yoga Mat', 45.00, 'A comfortable, lightweight, all-natural yoga mat');
-
-
-// addToCart(customer1, quinoa);
-// printCart(customer1);
-
-// addToCart(customer1, housePlant);
-// addToCart(customer1, housePlant);
-// addToCart(customer1, housePlant);
-// printCart(customer1);
-
-// addToCart(customer1, yogaMat);
-// addToCart(customer1, yogaMat);
-// addToCart(customer1, yogaMat);
-// printCart(customer1);
-
-// removeFromCart(customer1, housePlant);
-// printCart(customer1);
-
-// removeQuantityFromCart(customer1, yogaMat, 2);
-// printCart(customer1);
-
-
-
-
-/* - Create a class to describe a Item
-   - Be sure to encapsulate your class using private attributes with public getters/setters
-   - id as a string defaulted to a UUID
-   - name as a string
-   - price as an integer
-   - description as a string */
-
 class Item {
     private _id: string;
     private _name: string;
@@ -157,6 +39,33 @@ class Item {
     public set description(value: string) {
     this._description = value;
     }
+
+    /* - itemElement() - this will return an HTML Div Element.  This element will be something like a card that represents all the information about one item.  It will show the Items Name, Description and Price, and have an add to cart button. add the Eventlistener for addToCart here  */
+    itemElement(): HTMLDivElement {
+        let itemDiv = document.createElement('div');
+        itemDiv.id = this._id;
+        let itemName = document.createElement('p');
+        itemName.innerText = this._name;
+        let itemDescription = document.createElement('p');
+        itemDescription.innerText = this._description;
+        let itemPrice = document.createElement('p');
+        itemPrice.innerText = `$${this._price}`;
+        let addToCartButton = document.createElement('button');
+        addToCartButton.innerText = 'Add to Cart';
+        addToCartButton.id = `addToCart-${this._id}`;
+        addToCartButton.addEventListener('click', () => {
+            Shop.myUser.addToCart(this);
+            Shop.myUser.printCart();
+            // Shop.myUser.updateCart(Shop.myUser);
+        });
+        itemDiv.appendChild(itemName);
+        itemDiv.appendChild(itemDescription);
+        itemDiv.appendChild(itemPrice);
+        itemDiv.appendChild(addToCartButton);
+        return itemDiv;
+    }
+    
+
 }
 
 /* - Create a class to describe a User
@@ -254,6 +163,61 @@ class User {
         console.log(`The total cost of ${this.name}'s cart is $${this.cartTotal()}.`)
     }
 
+    /* static loginInUser() : Create a Static function that will retrieve the infomation from the HTML input field for nameand age. The function should return a new User created with the name and age, if a new and age was provided otherwise it can return null or undefined.  Note: This method is static because it doesn't need to work with a User instance.  And to call this static method somewhere else in code it would look like User.createUser() */
+    static loginUser(): User {
+        let name = (<HTMLInputElement>document.getElementById('name')).value;
+        let age = parseInt((<HTMLInputElement>document.getElementById('age')).value);
+        if (name && age) {
+            return new User(name, age);
+        } else {
+            return undefined;
+            console.log('undefined');
+        }
+    }
+
+    /*  - cartHTMLElement() : This will return an HTML Div Element.  This function will loop over your cart and create some HTML Code to layout your cart items in a formatted way to have the Name, Quantity and price of each item shown.  You will also create a button to Remove All or Remove Just One of these items from the cart. The event listenering for these buttons will be created ina  different function. Remember: You'll need to devise a way that each add and remove element is tied to a particular item (an easy way to do this is give every button an id based off the items UUIID and precede/follow the UUID with a tag that says whether the button is a remove one or remove all button)*/
+    cartHTMLElement(): HTMLDivElement {
+        let cartDiv = document.createElement('div');
+        cartDiv.id = 'cart';
+        for (let item of this._cart) {
+            let itemDiv = document.createElement('div');
+            itemDiv.id = item.id;
+            let itemName = document.createElement('p');
+            itemName.innerText = item.name;
+            let removeButton = document.createElement('button');
+            removeButton.innerText = 'Remove All';
+            removeButton.id = `removeAll-${item.id}`;
+            let removeOneButton = document.createElement('button');
+            removeOneButton.innerText = 'Remove One';
+            removeOneButton.id = `removeOne-${item.id}`;
+            itemDiv.appendChild(itemName);
+            itemDiv.appendChild(removeButton);
+            itemDiv.appendChild(removeOneButton);
+            cartDiv.appendChild(itemDiv);
+        }
+        return cartDiv;
+    }
+
+    /* -addRemoveEventListeners() - This function adds event listeners to your cart's Remove One/Remove All Buttons.  They will use the previously build removeQuantityFromCart and removeFromCart functions built in the prev. nights homework. */
+    addRemoveEventListeners(): void {
+        let cartDiv = document.getElementById('cart');
+        for (let item of this._cart) {
+            let removeButton = document.getElementById(`removeAll-${item.id}`);
+            let removeOneButton = document.getElementById(`removeOne-${item.id}`);
+            removeButton.addEventListener('click', () => {
+                this.removeFromCart(item);
+                cartDiv.innerHTML = '';
+                cartDiv.appendChild(this.cartHTMLElement());
+                this.addRemoveEventListeners();
+            });
+            removeOneButton.addEventListener('click', () => {
+                this.removeQuantityFromCart(item, 1);
+                cartDiv.innerHTML = '';
+                cartDiv.appendChild(this.cartHTMLElement());
+                this.addRemoveEventListeners();
+            });
+        }
+    }
 
 }
 
@@ -263,11 +227,7 @@ class User {
 */
 
 class Shop {
-    private _items: Item[];
-    // constructor() {
-    //     this._items = [];
-    // }
-
+    static myUser: User | undefined;
     public get items(): Item[] {
     return this._items;
     }
@@ -275,15 +235,53 @@ class Shop {
     this._items = value;
     }
 
-    /*- constructor
-        - The constructor will create three (3) Items and add them to the list of items in the shop */
-    constructor() {
+    /*  showItems() - This method will loop over all the shop items and add each items itemElement to the shop div of the HTML */
+    showItems(): void {
+        let shopDiv = document.getElementById('shop');
+        for (let item of this._items) {
+            shopDiv.appendChild(item.itemElement());
+        }
+    }
+
+    /* updateCart() - This method will create the cart contents and display them to the cart div in the HTML.  If the Cart is empty it should say the cart is empty, if the cart contains Items it will list all the cart items using the cartHTMLElement method and the addRemoveEventListeners function. */
+    updateCart(): void {
+        const cartDiv = document.getElementById('cart') as HTMLElement;
+        if (Shop.myUser.cart.length === 0) {
+            cartDiv.innerText = 'Your cart is empty.';
+        } else {
+            cartDiv.innerHTML = '';
+            cartDiv.appendChild(Shop.myUser.cartHTMLElement());
+            Shop.myUser.addRemoveEventListeners();
+        }
+
+    }
+
+    // static myUser property. This property will be either undefined (before the user "logins" or an actual User Object)  This will represent the person shopping at our store
+    // static myUser: User | undefined;
+    // static loginUser(event) - This will be a static function.  This will be attached to our "login/create user" button to be ran when the user logs in.  This method should create a user and save it to the myUser static property. If that user was created successfully it should then create the shop and cart elements on the page.
+    static loginUser(event): void {
+        event.preventDefault();
+        Shop.myUser = User.loginUser();
+        if (Shop.myUser) {
+            let shop = new Shop();
+            shop.showItems();
+            shop.updateCart(Shop.myUser);
+        }
+    }
+    // constructor() - This should now create SIX (6) items for your shop to sell.  It will also build out the shop div using the showItems() method, it will build out the cart section using the updateCart method.
+    constructor(
+        private _items: Item[];
+    ) {
         this._items = [];
         this._items.push(new Item('Quinoa', 25.00, 'Organic white quinoa, unwashed'));
         this._items.push(new Item('Monstera Leaf Plant', 10.00, 'A beautiful, large leafed house plant'));
         this._items.push(new Item('Cork Yoga Mat', 45.00, 'A comfortable, lightweight, all-natural yoga mat'));
+        this._items.push(new Item('Ceramic Coffee Mug', 15.00, 'A handcrafted ceramic coffee mug'));
+        this._items.push(new Item('Cotton Throw Blanket', 20.00, 'A soft, cozy cotton throw blanket'));
+        this._items.push(new Item('Soy Candle', 12.00, 'A hand-poured soy candle'));
+        this.showItems();
+        this.updateCart();
     }
-
 }
 
 // - User Class Methods:
@@ -295,31 +293,7 @@ function addToCart(user: User, item: Item):void{
     console.log(`${item.name} has been added to ${user.name}'s cart.`)
 }
 
-/*Create Driver Code to emulate a front end user
-- use the functions created to accomplish these tasks in order
-- Create a Shop
-- Create a User
-- Create some code to add items from the shop to the users cart, print the cart and remove all of a singular item from the cart and remove a quantity from a cart.
-- Verify all functions work as expected, if they don't figure out why not and fix your functions.*/
+/*## Driver Code
+    - We will need a little bit of code to run on page load. We can do this simply by writing this code outside of our classes and functions.  Thi code will add the loginUser method to the login/create user Button*/
 
-// Create a Shop
-let shop = new Shop();
-
-//Create a User
-let customer = new User('Adonai', 26);
-
-// Create some code to add items from the shop to the users cart, print the cart and remove all of a singular item from the cart and remove a quantity from a cart.
-addToCart(customer, shop.items[0]);
-addToCart(customer, shop.items[1]);
-addToCart(customer, shop.items[1]);
-addToCart(customer, shop.items[1]);
-addToCart(customer, shop.items[2]);
-addToCart(customer, shop.items[2]);
-addToCart(customer, shop.items[2]);
-customer.printCart();
-
-customer.removeFromCart(shop.items[1]);
-customer.printCart();
-
-customer.removeQuantityFromCart(shop.items[2], 2);
-customer.printCart();
+document.getElementById('login').addEventListener('click', Shop.loginUser);
